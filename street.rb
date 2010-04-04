fcdbe1a660b4f3107b5d3394a54ef4aeb891dd31
Ulrich Brodowsky <ulrich@brodowsky.com>
2010-04-04 20:37:12 +0200

    Alles fertig gemacht

--- street.rb
+++ street.rb
@@ -1,11 +1,19 @@
 # street.rb
 # author: Ulrich Brodowsky
 
+# Provisorial:
+
+STREET_TURN_COST_FACTOR = 0.5
+STREET_TURN_WIN_FACTOR = 0.5
+
 module Model
   
   # Represents a street in auf_aktien
 
   class Street
+
+    # initialize a new street with given parameters
+
     def initialize(information)
       @name = information[:name]
       @price = information[:price]
@@ -13,17 +21,71 @@ module Model
       @entry_costs = information[:entry_costs]
       @downgrade_cost = information[:downgrade_cost]
       @turned = false
-      @turnable = true
+      @turn_cost = (@price * STREET_TURN_COST_FACTOR).to_i
+      @turn_win = (@price * STREET_TURN_WIN_FACTOR).to_i
+      @owner = nil
+      @level = 0
     end
     
+    attr_reader :name, :price, :upgrade_cost, :entry_costs, :downgrade_cost, :turned, :turn_cost, :turn_win, :level 
+    attr_accessor :owner
+
+    # increases the level by one
+
+    def upgrade!
+      unless upgradable?
+        raise ShareException, "#{inspect} upgraded without possibility."
+      end
+      @level += 1
+    end
+
+    # gives the costs when someone enters the street with the level from now
+
+    def entry_cost
+      @entry_costs[level]
+    end
+
+    # decreases the level by one
+
+    def downgrade!
+      unless downgradable?
+        raise ShareException, "#{inspect} downgraded without possibility."
+      end
+      @level -= 1
+    end
+
+    # controlls if the street can be upgraded
+
+    def upgradable?
+      @level < 5 and @turned == false
+    end
+    
+    # controlls if the street can be downgraded
+
+    def downgradable?
+      @level > 0 and @turned == false
+    end
+    
+    # turns the stree, so that it can't be upgraded or so that it can be upgraded
+
+    def turn!
+      unless turnable?
+        raise ShareException, "#{inspect} turned without possibility."
+      end
+      @turned = !@turned
+    end
+
+    # Shows if the street can't be up and downgraded.
+
     def turned?
       @turned
     end
     
+    # Shows if the street can be turned.
+    
     def turnable?
-      @turnable
+      @level == 0
     end
-    attr_reader :name, :price, :upgrade_cost, :entry_costs, :downgrade_cost
   end
 
 end
--- street_spec.rb
+++ street_spec.rb
@@ -13,166 +13,176 @@ describe Model::Street do
     @street = Model::Street.new :name => @name, :price => @price, :upgrade_cost => @upgrade_cost, :entry_costs => @entry_costs, :downgrade_cost => @downgrade_cost
   end
   
-  it "should have the name we gave it" do
-    @street.name.should equal @name
-  end
-  
-  it "should have the price we gave it" do
-    @street.price.should equal @price
-  end
-  
-  it "should have the mortgage of half the price" do
-    @street.mortgage.should equal @price / 2
-  end
-  
-  it "should have the upgrade costs we gave" do
-    @street.upgrade_cost.should equal @upgrade_cost
-  end
-  
-  it "should have the hotel sell cost we gave" do
-    @street.downgrade_cost.should equal @downgrade_cost
+  describe "after creation" do
+    it "should have the name we gave it" do
+      @street.name.should equal @name
+    end
+    
+    it "should have the price we gave it" do
+      @street.price.should equal @price
+    end
+    
+    it "should have the turn cost of half the price" do
+      @street.turn_cost.should equal @price / 2
+    end
+    
+    it "should have the turn win of half the price" do
+      @street.turn_win.should equal @price / 2
+    end
+    
+    it "should have the upgrade costs we gave" do
+      @street.upgrade_cost.should equal @upgrade_cost
+    end
+    
+    it "should have the downgrade cost we gave" do
+      @street.downgrade_cost.should equal @downgrade_cost
+    end
+    
+    it "should have the entry costs we gave" do
+      @street.entry_costs.should equal @entry_costs
+    end
+    
+    it "should have level 0" do
+      @street.level.should equal 0
+    end
+    
+    it "shouldn't be turned" do
+      @street.should_not be_turned
+    end
+    
+    it "should be turnable" do
+      @street.should be_turnable
+    end
+    
+    it "should have no owner before we specified one" do
+      @street.owner.should be_nil
+    end
+
+    it "shouldn't be downgradable" do
+      @street.should_not be_downgradable
+    end
+    
+    it "should be upgradable" do
+      @street.should be_upgradable
+    end
   end
 
-  it "should have the entry costs we gave" do
-    @street.entry_costs.should equal @entry_costs
-  end
-  
-  it "should have no owner before we specified one" do
-    @street.owner.should be_nil
-  end
-  
-  it "should have the owner we gave" do
-    @street.owner = @owner
-    @street.owner.should equal @owner
-  end    
+  describe "after invalid operations" do
+    it "should raise an exception when downgraded after creation" do
+      lambda {
+        @street.downgrade!
+      }.should_raise ShareException
+    end
 
-  it "should have level 0 when we created it" do
-    @street.level.should equal 0
-  end
-
-  it "should have the level 1 costs with level 1"  do
-    @street.upgrade!
-    @street.entry_cost.should equal @entry_costs[1]
-  end
-
-  it "shouldn't be turned after creation" do
-    @street.should_not be_turned
-  end
-  
-  it "should be turnable after creation" do
-    @street.should be_turnable
-  end
-  
-  it "should be turnable with level 0(after 1 upgrade and 1 downgrade)" do
-    @street.upgrade!
-    @street.downgrade!    
-    @street.should be_turnable
-  end
-  
-  it "shouldn't be turnable with level 2" do
-    @street.upgrade!
-    @street.upgrade!
-    @street.should_not be_turnable
-  end
-  
-  it "should be turned after turning" do
-    @street.turn!
-    @street.should be_turned
-  end
-  
-  it "shouldn't be turned after turning 2 times" do
-    @street.turn!
-    @street.turn!
-    @street.should_not be_turned
-  end
-  
-  it "should have level 2 after 2 upgrades" do
-    @street.upgrade!
-    @street.upgrade!
-    @street.level.should equal 2
-  end
-  
-  it "should have level 1 after 2 upgrades and one downgrade" do
-    @street.upgrade!
-    @street.upgrade!
-    @street.downgrade!    
-    @street.level.should equal 1
-  end
-
-  it "shouldn't be upgradable if we upgrade 5 times" do
-    5.times {@street.upgrade!}
-    @street.should_not be_upgradable
-  end
-
-  it "should be downgradable if we upgrade 5 times" do
-    5.times {@street.upgrade!}
-    @street.should be_downgradable
-  end
-
-  it "shouldn't be downgradable when it is just created" do
-    @street.should_not be_downgradable
-  end
-
-  it "should be upgradable when it is just created" do
-    @street.should be_upgradable
-  end
-
-  it "shouldn't be downgradable after upgrade and downgrade 2 times" do
-    @street.upgrade!
-    @street.upgrade!
-    @street.downgrade!        
-    @street.downgrade!        
-    @street.should_not be_downgradable
-  end
-
-  it "should upgradable after upgrade and downgrade 2 times" do
-    @street.upgrade!
-    @street.upgrade!
-    @street.downgrade!        
-    @street.downgrade!        
-    @street.should be_upgradable
-  end
-
-  it "should raise an exception if we upgrade 6 times(or more)" do
+    it "should raise an exception if we upgrade 6 times(or more)" do
+      lambda {
+        6.times {@street.upgrade!}
+      }.should_raise ShareException
+    end
+    
+    it "should raise an exception if we upgrade 6 times(or more)(after 1 upgrade and 1 downgrade)" do
     lambda {
-      6.times {@street.upgrade!}
-    }.should_raise ShareException
+        4.times {@street.upgrade!}
+        @street.downgrade!
+        3.times {@street.upgrade!} 
+      }.should_raise ShareException
+    end
+    
+    it "should raise an exception if we upgrade 2 times and want to turn it" do
+      lambda {
+        2.times {@street.upgrade!}
+        @street.turn!
+      }.should_raise ShareException
+    end
+    
+    it "should raise an exception if we upgrade when it is turned" do
+      lambda {
+        @street.turn!
+        @street.upgrade!
+      }.should_raise ShareException
+    end
+    
+    it "should raise an exception if we downgrade from level 0(after 1 upgrade and 1 downgrade)" do
+      lambda {
+        @street.upgrade!
+        @street.downgrade!
+        @street.downgrade!
+      }.should_raise ShareException
+    end
+    
   end
 
-  it "should raise an exception if we upgrade 6 times(or more)(after 1 upgrade and 1 downgrade)" do
-    lambda {
-      4.times {@street.upgrade!}
-      @street.downgrade!
-      3.times {@street.upgrade!} 
-    }.should_raise ShareException
-  end
-
-  it "should raise an exception if we upgrade 2 times and want to turn it" do
-    lambda {
-      2.times {@street.upgrade!}
-      @street.turn!
-    }.should_raise ShareException
-  end
-
-  it "should raise an exception if we downgrade from level 0" do
-    lambda {
-      @street.downgrade!
-    }.should_raise ShareException
-  end
-
-  it "should raise an exception if we upgrade when it is turned" do
-    lambda {
-      @street.turn!
+  describe "in any situation" do
+    it "should have the owner we gave" do
+      @street.owner = @owner
+      @street.owner.should equal @owner
+    end    
+    
+    it "should have the level 1 costs with level 1"  do
       @street.upgrade!
-    }.should_raise ShareException
-  end
-
-  it "should raise an exception if we downgrade from level 0(after 1 upgrade and 1 downgrade)" do
-    lambda {
+      @street.entry_cost.should equal @entry_costs[1]
+    end
+    
+    it "should be turnable with level 0(after 1 upgrade and 1 downgrade)" do
       @street.upgrade!
-      @street.downgrade!
-      @street.downgrade!
-    }.should_raise ShareException
+      @street.downgrade!    
+      @street.should be_turnable
+    end
+    
+    it "shouldn't be turnable with level 2" do
+      @street.upgrade!
+      @street.upgrade!
+      @street.should_not be_turnable
+    end
+    
+    it "should be turned after turning" do
+      @street.turn!
+      @street.should be_turned
+    end
+    
+    it "shouldn't be turned after turning 2 times" do
+      @street.turn!
+      @street.turn!
+      @street.should_not be_turned
+    end
+    
+    it "should have level 2 after 2 upgrades" do
+      @street.upgrade!
+      @street.upgrade!
+      @street.level.should equal 2
+    end
+    
+    it "should have level 1 after 2 upgrades and one downgrade" do
+      @street.upgrade!
+      @street.upgrade!
+      @street.downgrade!    
+      @street.level.should equal 1
+    end
+    
+    it "shouldn't be upgradable if we upgrade 5 times" do
+      5.times {@street.upgrade!}
+      @street.should_not be_upgradable
+    end
+    
+    it "should be downgradable if we upgrade 5 times" do
+      5.times {@street.upgrade!}
+      @street.should be_downgradable
+    end
+    
+    it "shouldn't be downgradable after upgrades and downgrades 2 times" do
+      @street.upgrade!
+      @street.upgrade!
+      @street.downgrade!        
+      @street.downgrade!        
+      @street.should_not be_downgradable
+    end
+    
+    it "should upgradable after upgrades and downgrades 2 times" do
+      @street.upgrade!
+      @street.upgrade!
+      @street.downgrade!        
+      @street.downgrade!        
+      @street.should be_upgradable
+    end
   end
-
 end
